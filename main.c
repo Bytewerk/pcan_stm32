ceaf9d9a4936d26266db70cc79de217bc077af98
Hubert Denkmair <hubert@denkmair.de>
2015-07-25 22:59:57 +0200

    set clocking to suit pcan pro can timings

--- main.c
+++ main.c
@@ -6,13 +6,30 @@
  */
 
 #include <libopencm3/stm32/rcc.h>
+#include <libopencm3/stm32/flash.h>
 
 #include "config.h"
 #include "usb_pcan.h"
 #include "systime.h"
 
 int main(void) {
-	rcc_clock_setup_hse_3v3(&hse_8mhz_3v3[CLOCK_3V3_120MHZ]);
+
+	clock_scale_t clkcfg_96mhz = {
+		.pllm = 4,
+		.plln = 192,
+		.pllp = 4,
+		.pllq = 8,
+		.flash_config = FLASH_ACR_ICE | FLASH_ACR_DCE | FLASH_ACR_LATENCY_3WS, // TODO check if this is sane (seems to work for 48MHz as well as 120MHz)
+		.hpre = 1,
+		.ppre1 = 4,
+		.ppre2 = 2,
+		.power_save = 1,
+		.apb1_frequency = 24000000,
+		.apb2_frequency = 48000000
+	};
+
+	rcc_clock_setup_hse_3v3(&clkcfg_96mhz);
+	//rcc_clock_setup_hse_3v3(&hse_8mhz_3v3[CLOCK_3V3_120MHZ]);
 	systime_setup();
 	usb_pcan_init();
 
